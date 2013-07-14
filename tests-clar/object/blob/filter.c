58d68205cdebe159e246b03c51de61fe428d8bde
crazymaster <ken.i54k@gmail.com>
2013-07-14 18:20:23 +0900

    Fix debug message

--- tests-clar/object/blob/filter.c
+++ tests-clar/object/blob/filter.c
@@ -93,13 +93,13 @@ void test_object_blob_filter__stats(void)
 		cl_git_pass(git_blob__getbuf(&buf, blob));
 		git_buf_text_gather_stats(&stats, &buf, false);
                 if (i == NUM_TEST_OBJECTS - 2) {
-                  cl_check_pass(printf("%d", stats.bom));
-                  cl_check_pass(printf("%d", stats.nul));
-                  cl_check_pass(printf("%d", stats.cr));
-                  cl_check_pass(printf("%d", stats.lf));
-                  cl_check_pass(printf("%d", stats.crlf));
-                  cl_check_pass(printf("%d", stats.printable));
-                  cl_check_pass(printf("%d", stats.nonprintable));
+                  cl_assert(printf("%d", stats.bom));
+                  cl_assert(printf("%d", stats.nul));
+                  cl_assert(printf("%d", stats.cr));
+                  cl_assert(printf("%d", stats.lf));
+                  cl_assert(printf("%d", stats.crlf));
+                  cl_assert(printf("%d", stats.printable));
+                  cl_assert(printf("%d", stats.nonprintable));
                 }
 		cl_assert(memcmp(&g_stats[i], &stats, sizeof(stats)) == 0);
 		git_blob_free(blob);
